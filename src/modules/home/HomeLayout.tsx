3d49bf0391b741372c199504217e17c35323b5fa
Chamuditha S.M.A <87629269+IT21024122@users.noreply.github.com>
2025-08-15 00:04:21 +0530

    Update HomeLayout.tsx

--- src/modules/home/HomeLayout.tsx
+++ src/modules/home/HomeLayout.tsx
@@ -34,7 +34,7 @@ const HomeLayout = () => {
           </NavBarActions>
           <NavBarActions>
             <a
-              href={'https://github.com/sadanandpai/resume-builder'}
+              href={'https://github.com/esume-builder'}
               target="_blank"
               rel="noopener noreferrer"
             >
